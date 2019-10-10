c48143894740045d35dbfcadab31a3c84c6782c8
HaiyueChen <chenhaiyue2010@hotmail.com>
2019-10-10 21:44:49 +0200

    started on assignment c

--- code/assignments.py
+++ code/assignments.py
@@ -4,15 +4,18 @@
 from normalization import Normalizer, BrainDeadNormalizer
 from tokenization import BrainDeadTokenizer
 from corpus import Document, InMemoryDocument, Corpus, InMemoryCorpus
-from invertedindex import Posting, InMemoryInvertedIndex
+from invertedindex import Posting, InvertedIndex, InMemoryInvertedIndex
 from traversal import PostingsMerger
 from suffixarray import SuffixArray
 from ahocorasick import Trie, StringFinder
+from ranking import BrainDeadRanker
+from searchengine import SimpleSearchEngine
+from itertools import product, combinations_with_replacement
+from typing import Iterator
 import re
 import sys
 import tracemalloc
 import inspect
-import timeit
 
 
 def assignment_a_inverted_index_1():
@@ -138,12 +141,10 @@ def assignment_b_suffixarray_1():
     print("LOADING...")
     corpus = InMemoryCorpus("../data/cran.xml")
     print("INDEXING...")
-    indexing_start = timeit.default_timer()
     engine = SuffixArray(corpus, ["body"], normalizer, tokenizer)
-    indexing_stop = timeit.default_timer()
-    print(f"Indexing time: {indexing_stop - indexing_start}")
     results = []
     hit_count = 5
+
     # Callback for receiving matches.
     def match_collector(match):
         results.append(match)
@@ -225,20 +226,15 @@ def assignment_b_suffixarray_2():
         # excessive, most likely the implementation is copying strings or doing other silly stuff instead
         # of working with buffer indices. The naive reference implementation is not in any way optimized,
         # and uses about 1.5 MB of memory on this corpus.
-        print("Indexing...")
-        # tracemalloc.start()
-        # snapshot1 = tracemalloc.take_snapshot()
-        start = timeit.default_timer()
+        tracemalloc.start()
+        snapshot1 = tracemalloc.take_snapshot()
         engine = SuffixArray(TestCorpus(), fields, TestNormalizer(), BrainDeadTokenizer())
-        # snapshot2 = tracemalloc.take_snapshot()
-        # for statistic in snapshot2.compare_to(snapshot1, "filename"):
-        #     if statistic.traceback[0].filename == inspect.getfile(SuffixArray):
-        #         assert statistic.size_diff < 2000000, f"Memory usage is {statistic.size_diff}"
-        # tracemalloc.stop()
-        stop = timeit.default_timer()
-        print(f"Indexing time: {stop - start}")
+        snapshot2 = tracemalloc.take_snapshot()
+        for statistic in snapshot2.compare_to(snapshot1, "filename"):
+            if statistic.traceback[0].filename == inspect.getfile(SuffixArray):
+                assert statistic.size_diff < 2000000, f"Memory usage is {statistic.size_diff}"
+        tracemalloc.stop()
         results = []
-        print("Done indexing")
 
         def process(m):
             results.append((m['document'].document_id, m['score']))
@@ -265,6 +261,7 @@ def assignment_b_suffixarray_2():
             engine.evaluate(query, {'hit_count': 10}, process)
             assert results == expected
 
+
 def assignment_b_stringfinder():
 
     # Use these throughout below.
@@ -276,16 +273,13 @@ def assignment_b_stringfinder():
     for s in ["romerike", "apple computer", "norsk", "norsk ørret", "sverige", "ørret", "banan"]:
         trie.add(s, tokenizer)
     finder = StringFinder(trie, tokenizer)
-    buffer = "det var en gang en norsk ørret fra romerike som likte abba fra sverige"
-    
-    # print(trie)
-    # input()
+    buffer = "det var en gang en norsk  ørret fra romerike som likte abba fra sverige"
     print("SCANNING...")
     results.clear()
     finder.scan(buffer, lambda m: results.append(m))
     print("Buffer \"" + buffer + "\" contains", results)
     assert [m["match"] for m in results] == ["norsk", "norsk ørret", "ørret", "romerike", "sverige"]
-    input("Enter to continue")
+
     # Find all MeSH terms that occur verbatim in some selected Cranfield documents! Since MeSH
     # documents are medical terms and the Cranfield documents have technical content, the
     # overlap probably isn't that big.
@@ -309,31 +303,219 @@ def assignment_b_stringfinder():
         assert [m["match"] for m in results] == expected_matches
 
 
-def test_string_finder():
-    tokenizer = BrainDeadTokenizer()
-    trie = Trie()
-    for s in ["a", "a b"]:
-        trie.add(s, tokenizer)
-    finder = StringFinder(trie, tokenizer)
-    buffer = "a a b"
-    print("SCANNING...")
-    results = []
-    finder.scan(buffer, lambda m: results.append(m))
-    print("Buffer \"" + buffer + "\" contains", results)
-    # print([m["match"] for m in results])
-    assert [m["match"] for m in results] == ["a", "a", "a b"]
-
-
-
 def assignment_b():
-    # assignment_b_suffixarray_1()
-    # assignment_b_suffixarray_2()
-    # assignment_b_stringfinder()
-    test_string_finder()
+    assignment_b_suffixarray_1()
+    assignment_b_suffixarray_2()
+    assignment_b_stringfinder()
+
+
+
+def assignment_c_simplesearchengine_1():
+
+    # Use these throughout below.
+    normalizer = BrainDeadNormalizer()
+    tokenizer = BrainDeadTokenizer()
+
+    # Load and index MeSH terms.
+    print("LOADING...")
+    corpus = InMemoryCorpus("../data/mesh.txt")
+    print("INDEXING...")
+    inverted_index = InMemoryInvertedIndex(corpus, ["body"], normalizer, tokenizer)
+
+    # Do ranked retrieval, using a simple ranker.
+    engine = SimpleSearchEngine(corpus, inverted_index)
+    simple_ranker = BrainDeadRanker()
+    results = []
+
+    # Callback for receiving matches.
+    def match_collector(match):
+        results.append(match)
+        print("*** WINNER", match["score"], match["document"])
+
+    query = "polluTION Water"
+    for match_threshold in [0.1, 1.0]:
+        print(f"SEARCHING for '{query}' with match threshold {str(match_threshold)}...")
+        results.clear()
+        options = {"match_threshold": match_threshold, "hit_count": 10, "debug": False}
+        engine.evaluate(query, options, simple_ranker, match_collector)
+        assert len(results) == {0.1: 10, 1.0: 3}[match_threshold]
+        for (score, document_id) in [(match["score"], match["document"].document_id) for match in results[:3]]:
+            assert score == 2.0  # Both 'pollution' and 'water'.
+            assert document_id in [25274, 25275, 25276]
+        for score in [match["score"] for match in results[3:]]:
+            assert score == 1.0  # Only 'pollution' or 'water', but not both.
+
+
+def assignment_c_simplesearchengine_2():
+
+    # Use these throughout below.
+    normalizer = BrainDeadNormalizer()
+    tokenizer = BrainDeadTokenizer()
+    ranker = BrainDeadRanker()
+
+    # Used for comparing floating point numbers.
+    epsilon = 0.0001
+
+    # Create a dummy test corpus.
+    corpus = InMemoryCorpus()
+    words = (''.join(term) for term in product("bcd", "aei", "jkl"))
+    texts = (' '.join(word) for word in combinations_with_replacement(words, 3))
+    for text in texts:
+        corpus.add_document(InMemoryDocument(corpus.size(), {'a': text}))
+
+    # What we're testing.
+    engine = SimpleSearchEngine(corpus, InMemoryInvertedIndex(corpus, ["a"], normalizer, tokenizer))
+
+    # Where the callback will collect the matches.
+    results = []
+
+    # Callback that collects matches.
+    def collect(m):
+        results.append((m['score'], m['document'].document_id))
+
+    # Executes a query.
+    def search(q, t, n):
+        results.clear()
+        engine.evaluate(q, {'match_threshold': t, 'hit_count': n}, ranker, collect)
+
+    # Sorts the collected matches.
+    def sort_results():
+        results.sort(key=lambda e: e[1])
+        results.sort(key=lambda e: e[0], reverse=True)
+
+    # Test predicate.
+    def check_at(i, expected):
+        assert results[i] == expected
+
+    # Test predicate.
+    def check_range(indices, score, document_ids):
+        for i, d in zip(indices, document_ids):
+            check_at(i, (score, d))
+
+    # Test predicate.
+    def check_hits(n):
+        assert len(results) == n
+
+    # Run tests!
+    search('baj BAJ    baj', 1.0, 27)
+    check_hits(27)
+    check_at(0, (9.0, 0))
+    sort_results()
+    check_range(range(1, 27), 6.0, range(1, 27))
+    search('baj caj', 1.0, 100)
+    check_hits(27)
+    search('baj caj daj', 2/3 + epsilon, 100)
+    check_hits(79)
+    search('baj caj', 2/3 + epsilon, 100)
+    check_hits(100)
+    sort_results()
+    check_at(0, (3.0, 0))
+    check_range(range(4, 12), 2.0, range(1, 9))
+    check_range(range(12, 29), 2.0, range(10, 27))
+    check_at(29, (2.0, 35))
+    check_at(78, (2.0, 2531))
+    search('baj cek dil', 1.0, 10)
+    check_hits(1)
+    check_at(0, (3.0, 286))
+    search('baj cek dil', 2/3 + epsilon, 80)
+    check_hits(79)
+    sort_results()
+    check_at(0, (3.0, 13))
+    check_at(1, (3.0, 26))
+    check_at(2, (3.0, 273))
+    search('baj xxx yyy', 2/3 + epsilon, 100)
+    check_hits(0)
+    search('baj xxx yyy', 2/3 - epsilon, 100)
+    check_hits(100)
+
+
+def assignment_c_simplesearchengine_3():
+
+    # All accesses to posting lists are logged here.
+    accesses = []
+
+    # For testing.
+    class AccessLoggedIterator(Iterator[Posting]):
+
+        def __init__(self, term: str, wrapped: Iterator[Posting]):
+            self._term = term
+            self._wrapped = wrapped
+
+        def __next__(self):
+            posting = next(self._wrapped)
+            accesses.append((self._term, posting.document_id))
+            return posting
+
+    # For testing.
+    class AccessLoggedInvertedIndex(InvertedIndex):
+
+        def __init__(self, wrapped: InvertedIndex):
+            self._wrapped = wrapped
+
+        def get_terms(self, buffer: str) -> Iterator[str]:
+            return self._wrapped.get_terms(buffer)
+
+        def get_postings_iterator(self, term: str) -> Iterator[Posting]:
+            return AccessLoggedIterator(term, self._wrapped.get_postings_iterator(term))
+
+        def get_document_frequency(self, term: str) -> int:
+            return self._wrapped.get_document_frequency(term)
+
+    # Use these throughout below.
+    normalizer = BrainDeadNormalizer()
+    tokenizer = BrainDeadTokenizer()
+
+    # Load and index MeSH terms.
+    corpus = InMemoryCorpus("../data/mesh.txt")
+    inverted_index = AccessLoggedInvertedIndex(InMemoryInvertedIndex(corpus, ["body"], normalizer, tokenizer))
+
+    # Do ranked retrieval, using a simple ranker.
+    engine = SimpleSearchEngine(corpus, inverted_index)
+    simple_ranker = BrainDeadRanker()
+    query = "Water  polluTION"
+    options = {"match_threshold": 0.5, "hit_count": 1, "debug": False}
+    engine.evaluate(query, options, simple_ranker, lambda m: m)
+
+    # Expected posting list traversal ordering if the implementation chooses to evaluate this as "water pollution".
+    ordering1 = [('water', 3078),
+                 ('pollution', 788), ('pollution', 789), ('pollution', 790), ('pollution', 8079),
+                 ('water', 8635),
+                 ('pollution', 23837),
+                 ('water', 9379), ('water', 23234), ('water', 25265),
+                 ('pollution', 25274),
+                 ('water', 25266), ('water', 25267), ('water', 25268), ('water', 25269), ('water', 25270),
+                 ('water', 25271), ('water', 25272), ('water', 25273), ('water', 25274), ('water', 25275),
+                 ('pollution', 25275),
+                 ('water', 25276),
+                 ('pollution', 25276),
+                 ('water', 25277), ('water', 25278), ('water', 25279), ('water', 25280), ('water', 25281)]
+
+    # Expected posting list traversal ordering if the implementation chooses to evaluate this as "pollution water".
+    ordering2 = [('pollution', 788),
+                 ('water', 3078),
+                 ('pollution', 789), ('pollution', 790), ('pollution', 8079),
+                 ('water', 8635),
+                 ('pollution', 23837),
+                 ('water', 9379), ('water', 23234), ('water', 25265),
+                 ('pollution', 25274),
+                 ('water', 25266), ('water', 25267), ('water', 25268), ('water', 25269), ('water', 25270),
+                 ('water', 25271), ('water', 25272), ('water', 25273), ('water', 25274),
+                 ('pollution', 25275),
+                 ('water', 25275),
+                 ('pollution', 25276),
+                 ('water', 25276), ('water', 25277), ('water', 25278), ('water', 25279), ('water', 25280),
+                 ('water', 25281)]
+
+    # Check that the posting lists have been accessed in a way that's consistent with document-at-a-time traversal.
+    # Be somewhat robust to implementation details. This is a fairly strict test, and advanced (but valid)
+    # implementations that for some reason do lookaheads or whatever might fail.
+    assert accesses == ordering1 or accesses == ordering2
 
 
 def assignment_c():
-    pass
+    assignment_c_simplesearchengine_1()
+    assignment_c_simplesearchengine_2()
+    assignment_c_simplesearchengine_3()
 
 
 def assignment_d():
@@ -350,7 +532,7 @@ def main():
              "c": assignment_c,
              "d": assignment_d,
              "e": assignment_e}
-    assignments = sys.argv[1:] or ["b"]
+    assignments = sys.argv[1:] or ["a", "c"]
     for assignment in assignments:
         print("*** ASSIGNMENT", assignment.upper(), "***")
         tests[assignment.lower()]()
---
+++ code/searchengine.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# from collections import Counter
+# from utilities import Sieve
+from ranking import Ranker
+from corpus import Corpus
+from invertedindex import InvertedIndex
+from typing import Callable, Any, Dict
+from utilities import Sieve
+
+
+class SimpleSearchEngine:
+    """
+    A simple implementation of a search engine based on an inverted index, suitable for small corpora.
+    """
+
+    def __init__(self, corpus: Corpus, inverted_index: InvertedIndex):
+        self._corpus = corpus
+        self._inverted_index = inverted_index
+
+    def evaluate(self, query: str, options: dict, ranker: Ranker, callback: Callable[[dict], Any]) -> None:
+        """
+        Evaluates the given query, doing N-out-of-M ranked retrieval. I.e., for a supplied query having M terms,
+        a document is considered to be a match if it contains at least N <= M of those terms.
+
+        The matching documents are ranked by the supplied ranker, and only the "best" matches are returned to the
+        client via the supplied callback function.
+
+        The client can supply a dictionary of options that controls this query evaluation process: The value of
+        N is inferred from the query via the "match_threshold" (float) option, and the maximum number of documents
+        to return to the client is controlled via the "hit_count" (int) option.
+
+        The callback function supplied by the client will receive a dictionary having the keys "score" (float) and
+        "document" (Document).
+        """
+
+        # Print verbose debug information?
+        # debug = options.get("debug", False)
+        # print(query)
+
+        query_terms = self._get_query_terms(query)
+        threshold = options["match_threshold"] if "match_threshold" in options else 0
+        min_matches = max(1, min(len(query_terms), int(threshold * len(query_terms))))
+        hit_count = options["hit_count"] if "hit_count" in options else 0
+        if hit_count == 0:
+            return
+
+        sieve = Sieve(hit_count)
+
+        posting_iters = {}
+        for term in query_terms:
+            posting_iter = self._inverted_index.get_postings_iterator(term)
+            if posting_iter:
+                posting_iters[term] = posting_iter
+        
+        if len(posting_iters) < min_matches:
+            return
+        
+        current_postings = {}
+        for key in posting_iters:
+            itt = posting_iters[key]
+            posting = next(itt, None)
+            if not posting:
+                del posting_iters[key]
+            else:
+                current_postings[key] = posting
+    
+        last_matching_doc_id = -1
+        while len(posting_iters) >= min_matches:
+            min_doc_id = float("inf")
+            itter_to_advance = []
+            match_grouping = {}
+            for key in current_postings:
+                posting = current_postings[key]
+                doc_id = posting.document_id
+                if doc_id < min_doc_id:
+                    itter_to_advance.clear()
+                    min_doc_id = doc_id
+                    itter_to_advance.append(posting_iters[key])
+                elif doc_id == min_doc_id:
+                    itter_to_advance.append(posting_iters[key])
+
+                if doc_id in match_grouping:
+                    match_grouping[doc_id].append((key, posting))
+                else:
+                    match_grouping[doc_id] = [(key, posting)]
+
+            ## Check for match
+            for doc_id in match_grouping:
+                if doc_id == last_matching_doc_id:
+                    continue
+
+                if len(match_grouping[doc_id]) >= min_matches:
+                    last_matching_doc_id = doc_id
+                    ranker.reset(doc_id)
+                    match_tuples = match_grouping[doc_id]
+                    for match_tuple in match_tuples:
+                        multiplicity = query_terms[match_tuples[0]]
+                        ranker.update(term, multiplicity, match_tuples[1])
+                    score = ranker.evaluate()
+                    sieve.sift(score, (score, doc_id))
+
+            
+
+            input()
+
+        input()
+
+
+    def _get_query_terms(self, query) -> Dict[str, int]:
+        query_terms = self._inverted_index.get_terms(query)
+        query_terms_dict = {}
+        for term in query_terms:
+            if term in query_terms_dict:
+                query_terms_dict[term] += 1
+            else:
+                query_terms_dict[term] = 1
+        return query_terms_dict
